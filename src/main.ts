e5135d6734b3e5034df2b89ec38c28912a3817ba
Artem Mironchik <tawerka.power@gmail.com>
2024-03-15 19:36:01 +0300

    feat: configure swagger, change api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+import * as yaml from 'yaml';
+import { AppModule } from './app.module';
 
 const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const file = await fs.readFile(
+    path.join(__dirname, '../doc/api.yaml'),
+    'utf8',
+  );
+  const swaggerDocument = yaml.parse(file);
+
+  SwaggerModule.setup('api', app, swaggerDocument);
 
   app.useGlobalPipes(new ValidationPipe());
 
